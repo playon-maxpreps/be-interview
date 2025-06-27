49731f1d038cbd13d934141bcca71a5123e317c5
ken-singh <49081476+ken-singh@users.noreply.github.com>
2025-06-26 18:53:40 -0700

    Downgraded

--- Services/ISportService.cs
+++ Services/ISportService.cs
@@ -4,6 +4,7 @@ namespace DotNetInterviewApi.Services
 {
     public interface ISportService
     {
+        //Function with this name in SportService already exists
         Task<Sport[]> GetAllSportsAsync();
 
         Task<bool> IsSportOlympicAsync(int id);
